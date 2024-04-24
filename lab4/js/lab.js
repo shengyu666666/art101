5376cc8e7ceac1abf051535e94ae8ae988fcd0f4
shnegyu666666 <shengyu20030110@gmail.com>
2024-04-23 18:52:33 -0700

    lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,7 +1,10 @@
 // index.js - purpose and description here
+// Simple "Rock paper scissors" Game
+// An implementation of the game
 // Author: Your Name
+// Yu Sheng <ysheng15@ucsc.edu>
 // Date:
-
+// April 2024
 // Constants
 
 // Functions
@@ -9,7 +12,34 @@
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
-  // return results;
+  // Display the game menu
+    // Prompt user to start a new game or quit
+
+  // Initialize the game
+    // Set player and computer scores to 0
+  
+  // Game loop
+    // Start a new round
+      // Prompt player to choose Rock, Paper, or Scissors
+      // Validate player's input
+      // Generate computer's choice randomly
+      // Display player's and computer's choices
+      // Compare choices and determine the winner based on game rules
+        // If player wins, increment player's score
+        // If computer wins, increment computer's score
+        // If tie, no score change
+      // Display the result of the round
+      // Display current total scores
+   
+    // Check if the game has ended
+      // If either player reaches the winning score, end the game
+      // If not, start a new round
+
+  // Display the final game result
+    // Show the final scores
+    // Announce the winner
+
+  // Return to the game menu
 }
 
 function main() {
