71fd8de8ae12d6a15876adcfb35a8d1017af45aa
shnegyu666666 <shengyu20030110@gmail.com>
2024-05-02 20:31:52 -0700

    lab7

---
+++ lab7/js/lab.js
@@ -0,0 +1,23 @@
+// index.js - purpose and description here
+// Lab 7: Functions
+// Author: Your Name
+// Yu Sheng <ysheng15@ucsc.edu>
+// Date:
+// 2 May 2024
+// Constants
+
+// Functions
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+  // some code here
+  // return results;
+}
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+// let's get this party started
+main();
