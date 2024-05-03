da1eb05b43343b2c63ed5d8e0a765d4472d97393
shengyu666666 <shengyu20030110@gmail.com>
2024-05-02 21:48:26 -0700

    lab7

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -8,6 +8,33 @@
 
 // Functions
 
+// sortUserName - a function that takes user input and sorts the letters of their name
+function sortuserName() {
+    var userName = window.prompt("Hello, Please tell me your name!");
+    console.log("userName =", userName);
+
+    // verify if the input is valid
+    if (userName) {
+        userName = userName.toLowerCase(); // Convert to lowercase to ignore case differences
+        // split string to array
+        var nameArray = userName.split('');
+        console.log("nameArray =", nameArray);
+        // sort the array 
+        var namearraySort = nameArray.sort();
+        console.log("namearraySort =", namearraySort);
+        // join array back to a string
+        var nameSorted = namearraySort.join('');
+        console.log("nameSorted =", nameSorted);
+        //   userName.toLower().split("").sort().join("")
+        return nameSorted;
+    } else {
+        return "No name provided";
+    }
+}
+
+// output
+document.writeln("Oh, I've fixed your name: ", sortuserName(), "</br>");
+
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
