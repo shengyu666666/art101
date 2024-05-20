0333bd662edf149ef3d1a86ff452a921cbbfc632
shengyu666666 <shengyu20030110@gmail.com>
2024-05-19 19:29:49 -0700

    lab11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -14,18 +14,18 @@ function sortString(inputString) {
 // Click listener for the button
 $("#submit").click(function() {
   // Get values from input fields
-  const userName = $("#user-name").val();
-  const additionalInput1 = $("#additional-input1").val();
-  const additionalInput2 = $("#additional-input2").val();
+  const userName = $("#userName").val();
+  const userName2 = $("#userName2").val();
+  const userName3 = $("#userName3").val();
 
   // Sort the input strings
   const userNameSorted = sortString(userName);
-  const input1Sorted = sortString(additionalInput1);
-  const input2Sorted = sortString(additionalInput2);
+  const userName2Sorted = sortString(userName2);
+  const userName3Sorted = sortString(userName3);
 
   // Append new sorted names to the output div
   // This ensures not to replace existing content, but rather add to it
   $("#output").append('<div class="text"><p>' + userNameSorted + '</p></div>');
-  $("#output").append('<div class="text"><p>' + input1Sorted + '</p></div>');
-  $("#output").append('<div class="text"><p>' + input2Sorted + '</p></div>');
+  $("#output").append('<div class="text"><p>' + userName2Sorted + '</p></div>');
+  $("#output").append('<div class="text"><p>' + userName3Sorted + '</p></div>');
 });
